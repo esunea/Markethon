54c53cc64c9cbd0165123d9f0f1607eac42c79e4
esunea <paul.esusen@gmail.com>
2017-11-07 16:18:38 +0100

    modif dossiers, début PHP

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel='stylesheet' href='style/style.css'>
+  <title>Home</title>
+</head>
+<body>
+  <header>
+  <img class="logo" src="images/logo.png" alt="logo markethon(moche)">
+  <?php
+    echo"echo";
+     include("nav.html");
+  ?>
+<form action="profil.html">
+    <input type="text" placeholder="Login"><br/>
+    <input type="password" placeholder="Pass"><br/>
+    <input type="submit">
+    
+  </form>
+</header>
+  
+    
+    
+  
+</body>
+</html>
